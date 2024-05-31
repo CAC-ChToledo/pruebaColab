91f39e40388f68050375b017128d7334cf923f2e
CAC-ChToledo <cristian.toledo@bue.edu.ar>
2024-05-31 15:01:11 -0300

    Creación main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+def validarUsuario(nombre, clave):
+    pass
+
+opcion = ''
+menu = """
+*** Menú ***
+1 - Ingresar
+2 - Salir
+"""
+
+while opcion != "2":
+    print(menu)
+    opcion = input("Su opción >>> ")
+    
