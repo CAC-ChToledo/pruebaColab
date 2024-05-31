aae43ac7674890e0c8c079ae12254e2bbdff5811
ChTole <ch.ariel.tole@gmail.com>
2024-05-31 15:34:22 -0300

    Menú - ver cls

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ while opcion != "2":
     print(menu)
     opcion = input("Su opción >>> ")
     
+    if opcion == "1":
+        cargarUsuario()
+    elif opcion == "2":
+        print("Hasta luego!")
+    else:
+        print("Opción incorrecta!")
+        input("ENTER para continuar.")
+        os.system('cls')
